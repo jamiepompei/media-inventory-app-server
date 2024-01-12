fa86dfb31fa2f3d2d8b9ae76e5e2357d0af79082
Jamie Pompei <nicole.lynn.pompei@gmail.com>
2024-01-11 20:45:43 -0500

    initial commit

---
+++ app-server/src/test/java/com/inventory/app/server/AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.app.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
