c9d83e47c49395acfe4a89834d712006cb0079de
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-01-24 02:11:07 -0500

    fixed mapper, book controller, and DTO and pojos

--- app-server/src/main/java/com/inventory/app/server/mapper/BookMapper.java
+++ app-server/src/main/java/com/inventory/app/server/mapper/BookMapper.java
@@ -1,35 +1,90 @@
 package com.inventory.app.server.mapper;
 
-import com.inventory.app.server.config.converter.StringListConverter;
+import com.inventory.app.server.config.MediaInventoryAdditionalAttributes;
 import com.inventory.app.server.entity.Book;
+import com.inventory.app.server.entity.payload.request.MediaId;
 import com.inventory.app.server.entity.payload.request.MediaRequest;
+import com.inventory.app.server.entity.payload.response.MediaResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = StringListConverter.class)
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Mapper
 public interface BookMapper {
 
     BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
 
     @Mapping(source = "mediaId.id", target = "id")
+    @Mapping(source = "mediaId.version", target = "version")
     @Mapping(source = "mediaId.title", target = "title")
-    @Mapping(source = "mediaId.format", target = "")
-    @Mapping(source = "mediaId.genre", target = "")
+    @Mapping(source = "mediaId.format", target = "format")
+    @Mapping(source = "mediaId.genre", target = "genre")
     @Mapping(source = "mediaId.collectionName", target = "collectionName")
-    @Mapping(source = "additionalAttributes.authors", target = "authors", qualifiedByName = "stringListConverter")
-    @Mapping(source = "additionalAttributes.copyrightYear", target = "copyrightYear")
-    @Mapping(source = "additionalAttributes.edition", target = "edition")
+    @Mapping(source = "additionalAttributes", target = "authors", qualifiedByName = "mapAuthors")
+    @Mapping(source = "additionalAttributes", target = "copyrightYear", qualifiedByName = "mapCopyrightYear")
+    @Mapping(source = "additionalAttributes", target = "edition", qualifiedByName = "mapEdition")
     Book mapMediaRequestToBook(MediaRequest mediaRequest);
 
-    @Mapping(source = "id", target = "mediaId.id")
-    @Mapping(source = "title", target = "mediaId.title")
-    @Mapping(source = "format", target = "mediaId.format")
-    @Mapping(source = "genre", target = "mediaId.genre")
-    @Mapping(source = "collectionName", target = "mediaId.collectionName")
-    @Mapping(source = "authors", target = "additionalAttributes.authors", qualifiedByName = "stringListConverter")
-    @Mapping(source = "edition", target = "additionalAttributes.edition")
-    @Mapping(source = "copyrightYear", target = "additionalAttributes.copyrightYear")
-    Book mapBookToMediaResponse(Book book);
 
+    @Named("mapAuthors")
+    default List<String> mapAuthors(Map<String, Object> additionalAttributes) {
+      Object authorsValue = additionalAttributes.getOrDefault(MediaInventoryAdditionalAttributes.AUTHORS.getJsonKey(), Collections.emptyList());
+      @SuppressWarnings("unchecked")
+      List<String> authorsList = authorsValue instanceof List<?> ? (List<String>) authorsValue : Collections.emptyList();
+      return authorsList;
+    }
+
+    @Named("mapCopyrightYear")
+    default Integer mapCopyrightYear(Map<String, Object> additionalAttributes) {
+        return  additionalAttributes.containsKey(MediaInventoryAdditionalAttributes.COPYRIGHT_YEAR.getJsonKey()) ? (int) additionalAttributes.get(MediaInventoryAdditionalAttributes.COPYRIGHT_YEAR.getJsonKey()) : null;
+    }
+
+    @Named("mapEdition")
+    default Integer mapEdition(Map<String, Object> additionalAttributes) {
+        return  additionalAttributes.containsKey(MediaInventoryAdditionalAttributes.EDITION.getJsonKey()) ? (int) additionalAttributes.get(MediaInventoryAdditionalAttributes.EDITION.getJsonKey()) : null;
+    }
+
+    default MediaResponse mapBookToMediaResponseWithAdditionalAttributes(Book book) {
+        MediaResponse mediaResponse = new MediaResponse();
+        mediaResponse.setMediaId(mapBookIdToMediaId(book));
+        mediaResponse.setAdditionalAttributes(mapBookToAdditionalAttributes(book));
+        return mediaResponse;
+    }
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "version", target = "version")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "format", target = "format")
+    @Mapping(source = "genre", target = "genre")
+    @Mapping(source = "collectionName", target = "collectionName")
+    MediaId mapBookIdToMediaId(Book book);
+
+    @Named("mapBookToAdditionalAttributes")
+    default ConcurrentHashMap<String, Object> mapBookToAdditionalAttributes(Book book) {
+        ConcurrentHashMap<String, Object> additionalAttributes = new ConcurrentHashMap<>();
+
+        // Map authors if available
+        if (book.getAuthors() != null && !book.getAuthors().isEmpty()) {
+            additionalAttributes.put(MediaInventoryAdditionalAttributes.AUTHORS.getJsonKey(), book.getAuthors());
+        }
+
+        // Map edition if available
+        if (book.getEdition() != null) {
+            additionalAttributes.put(MediaInventoryAdditionalAttributes.EDITION.getJsonKey(), book.getEdition());
+        }
+
+        // Map copyright year if available
+        if (book.getCopyrightYear() != null){
+            additionalAttributes.put(MediaInventoryAdditionalAttributes.COPYRIGHT_YEAR.getJsonKey(), book.getCopyrightYear());
+        }
+
+        return additionalAttributes;
+    }
 }
+
