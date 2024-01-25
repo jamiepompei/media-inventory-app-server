35955da2bd4822d549523e79a8e5ce141ec93241
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-01-25 17:07:49 -0500

    updated create and updated enpoints and service layers

--- app-server/src/main/java/com/inventory/app/server/service/media/BookService.java
+++ app-server/src/main/java/com/inventory/app/server/service/media/BookService.java
@@ -1,13 +1,15 @@
 package com.inventory.app.server.service.media;
 
-import com.google.common.base.Preconditions;
+import com.inventory.app.server.config.MediaInventoryAdditionalAttributes;
 import com.inventory.app.server.entity.media.Book;
-import com.inventory.app.server.repository.*;
+import com.inventory.app.server.repository.IBaseDao;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,50 +29,66 @@ public class BookService {
         dao.setClazz(Book.class);
     }
 
-
     public List<Book> getAllBooksByCollectionTitle(String collectionTitle) {
-     return  dao.findAll();
+        return dao.findByField("collection_name", collectionTitle);
     }
 
-
-    public List<Book> getAllBooksByAuthor(String author){
-        Preconditions.checkNotNull(author);
-        return dao.findByField("author", author);
+    public List<Book> getAllBooksByAuthor(List<String> author) {
+        return dao.findByField(MediaInventoryAdditionalAttributes.AUTHORS.getJsonKey(), author);
     }
 
-
-    public List<Book> getAllBooksByGenre(String genre){
-        Preconditions.checkNotNull(genre);
+    public List<Book> getAllBooksByGenre(String genre) {
         return dao.findByField("genre", genre);
     }
-
 
     public List<Book> getAllBooks() {
         return dao.findAll();
     }
 
-    public Book getBookById(Long id){
+    public Book getBookById(Long id) {
         return dao.findOne(id);
     }
 
+    public Book create(Book book) {
+        if (bookAlreadyExists(book)) {
+            //todo throw exception
+        }
 
-    public Book create(Book book){
-       // Preconditions.checkNotNull(book);
-        Book bookToSave = new Book();
-        bookToSave.setAuthors(book.getAuthors());
-        bookToSave.setTitle(book.getTitle());
-        bookToSave.setEdition(book.getEdition());
-        bookToSave.setFormat(book.getFormat());
-        bookToSave.setCopyrightYear(book.getCopyrightYear());
-        bookToSave.setCollectionName(book.getCollectionName());
-        bookToSave.setVersion(1);
-        bookToSave.setGenre(book.getGenre());
+        Book bookToSave = cloneBook(book);
         bookToSave.setId(null);
-        return  dao.createOrUpdate(bookToSave);
+        bookToSave.setVersion(1);
+
+        return dao.createOrUpdate(bookToSave);
     }
 
-    public Book update(Book book) {
-        Preconditions.checkNotNull(book);
-        return dao.createOrUpdate(book);
+    public Book update(Book updatedBook) {
+        Book existingBook = getBookById(updatedBook.getId());
+        if (existingBook == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found, could not update " + updatedBook);
+        }
+        if (existingBook.equals(updatedBook)) {
+            //throw an exception here
+        }
+        updatedBook = cloneBook(existingBook, updatedBook);
+        updatedBook.setVersion(existingBook.getVersion() + 1);
+
+        return dao.createOrUpdate(updatedBook);
+    }
+
+    private Book cloneBook(Book book) {
+        Book clonedBook = new Book();
+        BeanUtils.copyProperties(book, clonedBook);
+        return clonedBook;
+    }
+
+    private Book cloneBook(Book existingBook, Book updatedBook) {
+        BeanUtils.copyProperties(updatedBook, existingBook);
+        return existingBook;
+    }
+
+    private boolean bookAlreadyExists(Book book) {
+        return getAllBooksByAuthor(book.getAuthors())
+                .stream()
+                .anyMatch(b -> book.getTitle().equals(b.getTitle()));
     }
 }
