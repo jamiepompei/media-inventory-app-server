f84aa0a8fd0b5dc9eb86c7a2e32e7532c9647301
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-01-26 13:33:12 -0500

    updated book and game entities

--- app-server/src/main/java/com/inventory/app/server/entity/media/Game.java
+++ app-server/src/main/java/com/inventory/app/server/entity/media/Game.java
@@ -1,15 +1,15 @@
 package com.inventory.app.server.entity.media;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
-@Entity(name = "Game")
+import java.util.Objects;
+
+@Entity(name = "game")
 @Table(name = "game")
-@Getter
-@Setter
+@Data
 public class Game extends Media {
     @ElementCollection(fetch = FetchType.LAZY)
     private List<String> consoles;
@@ -17,4 +17,37 @@ public class Game extends Media {
     private Integer numberOfPlayers;
     @Column(name = "release_date")
     private LocalDate releaseDate;
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "consoles=" + consoles +
+                ", numberOfPlayers=" + numberOfPlayers +
+                ", releaseDate=" + releaseDate +
+                " " +
+                super.toString() +
+                "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+
+        // Compare fields from the superclass (Media)
+        if(!super.equals(o)) return false;
+
+        return Objects.equals(getConsoles(), game.getConsoles()) &&
+                Objects.equals(getNumberOfPlayers(), game.getNumberOfPlayers()) &&
+                Objects.equals(getReleaseDate(), game.getReleaseDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                super.hashCode(),
+                getConsoles(),
+                getNumberOfPlayers(),
+                getReleaseDate());
+    }
 }
