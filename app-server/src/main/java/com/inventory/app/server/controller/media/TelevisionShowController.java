588226f56cde40e2d903b57ca84fdce2ba64fef7
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-03-20 23:08:09 -0400

    updates

--- app-server/src/main/java/com/inventory/app/server/controller/media/TelevisionShowController.java
+++ app-server/src/main/java/com/inventory/app/server/controller/media/TelevisionShowController.java
@@ -1,4 +1,4 @@
-package com.inventory.app.server.controller;
+package com.inventory.app.server.controller.media;
 
 import com.inventory.app.server.entity.media.TelevisionShow;
 import com.inventory.app.server.entity.payload.request.MediaRequest;
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -29,6 +30,7 @@ public class TelevisionShowController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER, 'ROLE_VIEW')")
     ResponseEntity<List<MediaResponse>> findAllTelevisionShows() {
         List<MediaResponse> responseList = televisionService.getAll().stream()
                 .map(t -> TelevisionShowMapper.INSTANCE.mapTelevisionShowToMediaResponseWithAdditionalAttributes(t))
@@ -37,6 +39,7 @@ public class TelevisionShowController {
     }
 
     @GetMapping(value = "/{episodes}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER, 'ROLE_VIEW')")
     ResponseEntity<List<MediaResponse>> findByEpisode(@PathVariable("episodes") final List<String> episodes) {
         if (episodes.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request. Writers cannot be empty.");
@@ -50,6 +53,7 @@ public class TelevisionShowController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER)")
     public ResponseEntity<MediaResponse> createTelevisionShow(@Valid @RequestBody final MediaRequest televisionShowRequest) {
         log.info("Received request to create resource: " + televisionShowRequest);
         TelevisionShow televisionShow = TelevisionShowMapper.INSTANCE.mapMediaRequestToTelevisionShow(televisionShowRequest);
@@ -60,6 +64,7 @@ public class TelevisionShowController {
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER)")
     public ResponseEntity<MediaResponse> updateTelevisionShow(@Valid @RequestBody final MediaRequest televisionShowRequest) {
         log.info("Received request to update resource: " + televisionShowRequest);
         TelevisionShow updatedTelevisionShow = TelevisionShowMapper.INSTANCE.mapMediaRequestToTelevisionShow(televisionShowRequest);
@@ -69,6 +74,7 @@ public class TelevisionShowController {
 
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER')")
     public ResponseEntity<MediaResponse> deleteTelevisionShow(@PathVariable("id") final Long id){
         if (id == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Request. Id cannot be null or empty.");
